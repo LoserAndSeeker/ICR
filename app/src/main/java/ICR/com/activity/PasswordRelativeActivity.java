457d7044df720309c7fc69bb30c01dac9dfc69d4
lonely <lonelywilliam@163.com>
2019-01-04 19:53:59 +0800

    change the layout of register.login,QRcode,passwordRelative

--- app/src/main/java/ICR/com/activity/PasswordRelativeActivity.java
+++ app/src/main/java/ICR/com/activity/PasswordRelativeActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import ICR.com.R;
 //忘记密码界面，至于为什么是relative，抄的模板，详情请对照系统设计文档
@@ -14,12 +16,31 @@ public class PasswordRelativeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.passwordrelative_layout);
-        Button button1 = (Button) findViewById(R.id.button_login1);
+        hindBar();
+        Button button1 = (Button) findViewById(R.id.button_finish);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        final EditText et_password = (EditText) findViewById(R.id.password_edit);
+        final ImageButton bt_change_mode = (ImageButton) findViewById(R.id.imageButton2);
+        bt_change_mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et_password.getInputType() == 128){//如果现在是显示密码模式
+                    et_password.setInputType(129);//设置为隐藏密码
+                //    bt_change_mode.setText("密码不可见");
+                }else {
+                    et_password.setInputType(128);//设置为显示密码
+                 //   bt_change_mode.setText("密码可见");
+                }
+                et_password.setSelection(et_password.getText().length());//设置光标的位置到末尾
+
+
+    }
+});
     }
 }
